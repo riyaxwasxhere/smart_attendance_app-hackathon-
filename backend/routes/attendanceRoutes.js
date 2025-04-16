36a5afc37e3af45474852bfd32a8b7331c4bc9a8
Purbasha Goswami <purbashagoswami2@gmail.com>
2025-04-16 21:15:57 +0530

    added totalClasses record route

--- backend/routes/attendanceRoutes.js
+++ backend/routes/attendanceRoutes.js
@@ -66,6 +66,85 @@ router.get("/:subject/:studentRoll/last30days", async (req, res) => {
   }
 });
 
+//To get all the attendance record of a particular student of a particular subject
+router.get("/:dept/:studentRoll/:subject", async (req, res) => {
+  try {
+    const { dept, studentRoll, subject } = req.params;
+    const record = await attendance.find({ dept, studentRoll, subject });
+    res.status(200).json(record);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+//To get all student record for a particular subject for today
+router.get("/:dept/:className/:subject/today", async (req, res) => {
+  try {
+    const { dept, className, subject } = req.params;
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0, 0);
+    const endOfDay = new Date();
+    endOfDay.setHours(23, 59, 59, 999);
+    const record = await attendance.find({
+      dept,
+      className,
+      subject,
+      createdAt: { $gte: startOfDay, $lte: endOfDay },
+    });
+    res.status(200).json(record);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+//To get all student record for a particular subject for last7days
+router.get("/:dept/:className/:subject/lastweek", async (req, res) => {
+  try {
+    const { dept, className, subject } = req.params;
+    const startDate = new Date();
+    startDate.setDate(startDate.getDate() - 7);
+    startDate.setHours(0, 0, 0, 0);
+    const endDate = new Date();
+    const record = await attendance.find({
+      dept,
+      className,
+      subject,
+      createdAt: { $gte: startDate, $lte: endDate },
+    });
+    res.status(200).json(record);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// to get total classes of a subject
+router.get("/totalClasses/:dept/:className/:subject", async (req, res) => {
+  try {
+    const { dept, className, subject } = req.params;
+    const totalClasses = await attendance.aggregate([
+      { $match: { dept: dept, className: className, subject: subject } },
+      {
+        $group: {
+          _id: {
+            dateTime: {
+              $dateToString: {
+                format: "%Y-%m-%d %H:%M:%S",
+                date: "$createdAt",
+              },
+            },
+          },
+        },
+      },
+      {
+        $count: "totalClasses",
+      },
+    ]);
+    res.status(200).json({ totalClasses: totalClasses[0]?.totalClasses || 0 });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // to add attendance-working
 router.post("/", async (req, res) => {
   try {
